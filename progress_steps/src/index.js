6ebdebf981c266cd4c255a77a605c8638a1e03a5
Assassinationrogue <saumyadip11@gmail.com>
2021-04-01 17:39:26 +0530

    perfect looking page

--- progress_steps/src/index.js
+++ progress_steps/src/index.js
@@ -7,16 +7,16 @@ import ReactDOM from 'react-dom';
 function getButtonText() {
     return 'Click on me!';
 }
-
+ // javascript will consider that html's for as javascript's for, so we replace that for with htmlFor
 const App = function() {
-    const buttonText = 'Click Me';
+    const buttonText = ['Hi', ' There'];
     return (
         <div>
-            <label className="label" for="name">
+            <label className="label" htmlFor="name"> 
                 Enter Name:
             </label>
             <input id="name" type="text" />
-            <button style={{backgroundColor:'blue', color:'white'}}>{getButtonText()}</button>
+            <button style={{backgroundColor:'blue', color:'white'}}>{buttonText}</button>
         </div>
     );
 };

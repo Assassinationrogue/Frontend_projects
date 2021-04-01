bef8c737752a0a1376c814b7b0d7fb055d466665
Assassinationrogue <saumyadip11@gmail.com>
2021-04-02 04:39:23 +0530

    added_script_and_style

---
+++ Navigation_bar/script.js
@@ -0,0 +1,12 @@
+const open = document.querySelector('#open');
+const close = document.querySelector('#close');
+const container = document.querySelector('.container');
+
+console.log(open)
+open.addEventListener('click', ()=>{
+    container.classList.add('show-nav');
+});
+
+close.addEventListener('click',()=>{
+    container.classList.remove('show-nav');
+})
